3b0246d256ff2e286b9b70ddf3a32ac3229a0590
haodev1202 <haodev1202@gmail.com>
2024-08-16 22:11:08 +0700

    update example callback

--- js/callback.js
+++ js/callback.js
@@ -3,7 +3,7 @@
 // Cả 2 điều trên được gọi là callback
 
 // Lý do phải sử dụng callback
-// Trong js thay vì chờ đợi phải hồi , js vẫn sẽ thực thi các lệnh tiếp theo , 
+// Trong js thay vì chờ đợi phải hồi , js vẫn sẽ thực thi các lệnh tiếp theo ,
 //đồng thời chờ đợi phản hồi từ các sự việc khác
 // example
 function first() {
@@ -19,13 +19,18 @@ second();
 
 // Do vậy để đảm bảo thực hiện đúng thứ tự thì ta cần sử dụng đến khái niệm callback
 // Nó sẽ đảm bảo code sẽ không hoạt động trước khi code khác hoàn thành việc thực thi
-function doHomeWork(subject , callback) {
+function doHomeWork(subject, callback) {
     console.log(`Start my ${subject} homework.`);
-    callback();
+    callback(() => {
+        console.log(1);
+    });
 }
 
-function alertFinished() {
-    alert('Finished my homework');
+function alertFinished(done) {
+    setTimeout(() => {
+        console.log("Finished my homework");
+        done();
+    }, 5000);
 }
 
-doHomeWork('math' , alertFinished);
+doHomeWork("math", alertFinished);
